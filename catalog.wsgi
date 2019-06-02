af5bace8742c6e2bfcbe1a1a8da2380afd313696
Filipe Calasans <filipe.calasans@gmail.com>
2019-06-02 16:39:53 -0400

    add wsgi file

---
+++ catalog.wsgi
@@ -0,0 +1,5 @@
+import sys
+
+sys.path.insert(0, '/home/catalog')
+
+from webserver import app as application
--- webserver.py
+++ webserver.py
@@ -216,6 +216,6 @@ def category():
 
 
 if __name__ == '__main__':
-	#app.debug = True
-	#app.run(use_reloader=False, host='0.0.0.0', port=5000)
-	app.run()
+	app.debug = True
+	app.run(use_reloader=False, host='0.0.0.0', port=5000)
+	#app.run()
